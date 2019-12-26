25497fc8d7e35a82d5bda1b494655fac9b1645e8
Mojtaba <mojtabamarashee@gmail.com>
2019-12-26 13:01:02 +0330

    test

---
+++ src/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Badge from '@material-ui/core/Badge';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFilter} from '@fortawesome/free-solid-svg-icons';
+import {faQuestionCircle} from '@fortawesome/free-solid-svg-icons';
+
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div style={{margin: '5px'}}>
+				<Badge badgeContent={this.props.badge} color="primary">
+					<FontAwesomeIcon
+						style={{margin: '10px 0 5px 0'}}
+						onClick={() => this.props.ChangeMode('table')}
+						icon={faFilter}
+						size="3x"
+						color="lightBlue"
+					/>
+				</Badge>
+				<FontAwesomeIcon
+					style={{margin: '0 0 5px 0', float: 'right'}}
+					onClick={null}
+					icon={faQuestionCircle}
+					size="3x"
+					color="lightBlue"
+				/>
+			</div>
+		);
+	}
+}
+export {Header};
---
+++ src/Sector.js
@@ -0,0 +1,31 @@
+
+class Sector extends React.Component {
+    render(){
+return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+      <List>
+        {emails.map(email => (
+          <ListItem button onClick={() => handleListItemClick(email)} key={email}>
+            <ListItemAvatar>
+              <Avatar className={classes.avatar}>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={email} />
+          </ListItem>
+        ))}
+
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="add account" />
+        </ListItem>
+      </List>
+    </Dialog>
+  )};
+}
+}
