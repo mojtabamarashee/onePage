4d08b5d7bdc224d0cca0b2079a6db460ce99e162
unknown <mojtabamarashee@gmail.com>
2020-01-15 21:25:43 +0330

    test

--- src/Swing.jsx
+++ src/Swing.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class Swing extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		let minP, maxP, min, max;
-		({minP, maxP, min, max} = this.props.data);
-		return (
-			<div style={{width: '100%', height: '5px'}}>
-				<div
-					style={{
-						margin: ((minP - min) / (max - min)) * 100 + '%',
-						backgroundColor: 'red',
-						width: minP < 0 ? (((maxP - minP) / (max - min)) * 100 + '%') : '0px',
-						height: '100%',
-					}}
-				/>
-			</div>
-		);
-	}
+  render() {
+    let minP, maxP, min, max, name;
+    ({minP, maxP, min, max, name} = this.props.data);
+    minP < (max + min) / 2
+      ? (console.log('name = ', name),
+        console.log('minP = ', minP),
+        console.log('maxP = ', maxP),
+        console.log('max = ', max),
+        console.log('min = ', min))
+      : null;
+    return (
+      <div style={{width: '100%', height: '10px'}}>
+        <div
+          style={{
+            margin:
+              minP < (max + min) / 2
+                ? ((minP - min) / (max - min)) * 100 + '%'
+                : null,
+            backgroundColor: 'red',
+              minP < (max + min) / 2
+                ? width: ((maxP - minP) / (max - min)) * 100 + '%'
+                : (width:'0px'),
+            height: '5px',
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export {Swing};
