04c9568e20b30896958a7161000f373239fa3381
unknown <mojtabamarashee@gmail.com>
2020-01-17 23:35:08 +0330

    test

---
+++ src/SymbolPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SymbolPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>hi</h1>;
+  }
+}
+export {SymbolPage};
