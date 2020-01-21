5da45719f616ffd358dcc4ce17adec71a2ec2ad0
Mojtaba <mojtabamarashee@gmail.com>
2020-01-21 10:27:07 +0330

    test

---
+++ src/Portfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Portfo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		let v = this.props.v;
+		return <div />;
+	}
+}
